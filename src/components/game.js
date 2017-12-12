3d05c3654dd5cab08fd6a4d49b8c5b4197b857ba
John Knotts <jhnnyk@gmail.com>
2017-12-11 18:04:07 -0700

    implement feedback via Redux 🎉

--- src/components/game.js
+++ src/components/game.js
@@ -6,47 +6,7 @@ import GuessSection from "./guess-section";
 import StatusSection from "./status-section";
 import InfoSection from "./info-section";
 
-import { restartGame } from "../actions";
-
 export class Game extends React.Component {
-  restartGame() {
-    this.props.dispatch(restartGame());
-  }
-
-  makeGuess(guess) {
-    guess = parseInt(guess, 10);
-    if (isNaN(guess)) {
-      this.setState({ feedback: "Please enter a valid number" });
-      return;
-    }
-
-    const difference = Math.abs(guess - this.props.correctAnswer);
-
-    let feedback;
-    if (difference >= 50) {
-      feedback = "You're Ice Cold...";
-    } else if (difference >= 30) {
-      feedback = "You're Cold...";
-    } else if (difference >= 10) {
-      feedback = "You're Warm.";
-    } else if (difference >= 1) {
-      feedback = "You're Hot!";
-    } else {
-      feedback = "You got it!";
-    }
-
-    this.setState({
-      feedback,
-      guesses: [...this.state.guesses, guess]
-    });
-
-    // We typically wouldn't touch the DOM directly like this in React
-    // but this is the best way to update the title of the page,
-    // which is good for giving screen-reader users
-    // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : "Hot or Cold";
-  }
-
   generateAuralUpdate() {
     const { guesses, feedback } = this.state;
 
@@ -80,7 +40,6 @@ export class Game extends React.Component {
           <GuessSection
             feedback={this.props.feedback}
             guessCount={guessCount}
-            onMakeGuess={guess => this.makeGuess(guess)}
           />
           <StatusSection
             guesses={this.props.guesses}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,10 +15,33 @@ export const gameReducer = (state = initialState, action) => {
         correctAnswer: Math.floor(Math.random() * 100) + 1
       };
     case actions.MAKE_GUESS:
-      return {
-        ...state,
-        guesses: [...state.guesses, action.guess]
-      };
+      const guess = parseInt(action.guess, 10);
+      if (isNaN(guess)) {
+        return { ...state, feedback: "Please enter a valid number" };
+      }
+
+      const difference = Math.abs(guess - state.correctAnswer);
+
+      let feedback;
+      if (difference >= 50) {
+        feedback = "You're Ice Cold...";
+      } else if (difference >= 30) {
+        feedback = "You're Cold...";
+      } else if (difference >= 10) {
+        feedback = "You're Warm.";
+      } else if (difference >= 1) {
+        feedback = "You're Hot!";
+      } else {
+        feedback = "You got it!";
+      }
+
+      // We typically wouldn't touch the DOM directly like this in React
+      // but this is the best way to update the title of the page,
+      // which is good for giving screen-reader users
+      // instant information about the app.
+      document.title = feedback ? `${feedback} | Hot or Cold` : "Hot or Cold";
+
+      return { ...state, feedback, guesses: [...state.guesses, action.guess] };
     default:
       return state;
   }
